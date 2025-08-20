d5bea7366e5a6955938efe56b1e8183ced05acfe
faisal-990 <sawezfaisals123@gmail.com>
2025-08-21 03:39:09 +0530

    refactored + working echo and ping commands

--- your_program.sh
+++ your_program.sh
@@ -8,17 +8,12 @@
 
 set -e # Exit early if any commands fail
 
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
+# Build the binary
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  go build -o /tmp/codecrafters-build-redis-go app/main.go
+  go build -o /tmp/codecrafters-build-redis-go ./cmd/main.go
 )
 
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
+# Run the binary
 exec /tmp/codecrafters-build-redis-go "$@"
+
